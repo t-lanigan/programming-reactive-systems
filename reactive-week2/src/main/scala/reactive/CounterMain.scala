337bd9ca25c633d836631fb95d28b7ec21206e82
Tyler Lanigan <tylerlanigan@gmail.com>
2019-10-05 15:47:09 -0700

    finally got example working

--- reactive-week2/src/main/scala/reactive/CounterMain.scala
+++ reactive-week2/src/main/scala/reactive/CounterMain.scala
@@ -4,8 +4,13 @@ import akka.actor.Actor
 import akka.actor.Props
 
 class CounterMain extends Actor {
+
+  // To run this file as is, you need to make a new run configuration with making class akka.Main and
+  // run arguements reactive.CounterMain. Don't forget to set the calsspath of module (reactive week-2)
   val counter = context.actorOf(Props[Counter], "counter")
 
+  counter ! "incr"
+  counter ! "incr"
   counter ! "incr"
   counter ! "incr"
   counter ! "get"
